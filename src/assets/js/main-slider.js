e0cceb2f7d06442be23695849647e9a28a981a1b
frontdevruso <frontdevruso@gmail.com>
2022-03-06 14:11:16 +0300

    half of slider

--- src/assets/js/main-slider.js
+++ src/assets/js/main-slider.js
@@ -1,13 +1,26 @@
-let mainSlider = new Swiper('.main-slider', {
-    direction: 'vertical',
-    slidesPerView: 1,
-    spaceBetween: 10,
-});
+if (document.querySelector('.main-slider')) {    
+    let mainSlider = new Swiper('.main-slider', {
+        direction: 'vertical',
+        slidesPerView: 1,
+        spaceBetween: 120,
+    
+        pagination: {
+            el: ".main-slider__navigation-fraction",
+            clickable: true,
+            type: "fraction",
+        },
 
-mainSlider.on('slidePrevTransitionStart', function() {
- console.log('Prev slide');
-});
+        navigation: {
+            nextEl: ".main-slider__navigation-arrow--prev",
+            prevEl: ".main-slider__navigation-arrow--next",
+        },
+    });
 
-mainSlider.on('slideNextTransitionStart', function() {
- console.log('Next slide');
-});
+    mainSlider.on('slidePrevTransitionStart', function() {
+        console.log('Prev slide');
+    });
+    
+    mainSlider.on('slideNextTransitionStart', function() {
+        console.log('Next slide');
+    });
+}
