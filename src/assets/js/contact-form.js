6a4e35c11fb27ce206fbc703eea78a963b2ea1f0
frontdevruso <frontdevruso@gmail.com>
2022-03-31 15:41:05 +0300

    contact form thx

--- src/assets/js/contact-form.js
+++ src/assets/js/contact-form.js
@@ -24,6 +24,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 formCheckboxWrapper.classList.remove('policy-error');
             }
         });
+
+        const thankYou = () => document.querySelector('.thx__message h2 span').innerHTML = `, ${formName.value}`;
     
         contactFormSubmitBtn.addEventListener('click', function(e) {
             e.preventDefault();
@@ -51,8 +53,9 @@ document.addEventListener("DOMContentLoaded", () => {
             } else { formCheckbox.classList.remove('policy-error') }
 
             if (errCount === 0) {
+                document.querySelector('.card360deg').classList.add('card360deg--active');
+                thankYou();
                 // HERE YOU CAN ADD A AJAX REQUEST TO SEND THE FORM
-                alert('The data sending was Succsefully!');
                 console.log(errCount);
             }
         });
