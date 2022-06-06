abb287253f2cd1bbba0ed160f64d49270dfebcb7
frontdevruso <frontdevruso@gmail.com>
2022-06-06 16:07:27 +0300

    fixing mm on IOS

--- src/assets/js/equipment-slider.js
+++ src/assets/js/equipment-slider.js
@@ -1,15 +1,15 @@
-let swiperEquipment = new Swiper(".equipment-slider", {
-    spaceBetween: 20,
-    autoHeight: true,
-
-    pagination: {
-        el: ".equipment-slider-fraction",
-        clickable: true,
-        type: "fraction",
-    },
-
-    navigation: {
-        nextEl: ".equipment-slider--next",
-        prevEl: ".equipment-slider--prev",
-    },
-});
+if(document.querySelector('.equipment-slider')) {
+    let swiperEquipment = new Swiper(".equipment-slider", {
+        spaceBetween: 20,
+        pagination: {
+            el: ".equipment-slider-fraction",
+            clickable: true,
+            type: "fraction",
+        },
+    
+        navigation: {
+            nextEl: ".equipment-slider--next",
+            prevEl: ".equipment-slider--prev",
+        },
+    });
+}
